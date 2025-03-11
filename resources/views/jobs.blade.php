68c8b7db1536f8911b339224f2b4c9bb93b0677b
ChristianSheldon <christiansheldon@hotmail.com>
2025-03-11 13:40:56 -0400

    changes the way jobs are viewed, also sets a catch for null employers

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,19 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+            <a>
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="font-bold">{{ $job->employer ? $job->employer->name: 'N/A' }} {{-- added catch for null employers --}}
+
+                    </div>
+                    <div>
+                        <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                    </div>
+
                 </a>
-            </li>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
